90538908143eef9e559dac9038c256191df438ea
Guiny-Time <1084926982@qq.com>
2024-06-07 15:09:00 +0800

    Site updated: 2024-06-07 15:08:59

--- next/js/fold_action.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(document).on('click', '.fold_hider', function(){
-        $('>.fold', this.parentNode).slideToggle();
-        $('>:first', this).toggleClass('open');
-    });
-    //默认情况下折叠
-    $("div.fold").css("display", "none");
-});
